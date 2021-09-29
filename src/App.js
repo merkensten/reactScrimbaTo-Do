613eafc3dbb17456db8e59e3825c3126beb350b2
Mattis <m.erkensten@icloud.com>
2021-09-29 13:05:18 +0200

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li className="to-do"><input type="checkbox"></input><p>Todo - 1</p></li>
+        <li className="to-do"><input type="checkbox"></input><p>Todo - 2</p></li>
+        <li className="to-do"><input type="checkbox"></input><p>Todo - 3</p></li>
+        <li className="to-do"><input type="checkbox"></input><p>Todo - 4</p></li>
+      </ul>
     </div>
   );
 }
